2d02104e704cbf9bf448a824f0b03d3c37db3cdf
Oleg1989 <ivanishin9891@gmail.com>
2021-11-03 22:50:12 +0200

    First commit

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test-task',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Message } from '@test-task-existek/api-interfaces';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AppService {
+  constructor(private sequelize: Sequelize) { }
   getData(): Message {
     return { message: 'Welcome to api!' };
   }
